bbeb0b22a7649ee65f5ea7eaca4b0d8f0688647b
Mickaël Canouil <mickael.canouil@cnrs.fr>
2019-12-06 13:28:24 +0100

    add conflicts

---
+++ R/conflicts.R
@@ -0,0 +1,152 @@
+.onAttach <- function(...) {
+  crayon::num_colors(TRUE)
+  if (!"package:conflicted" %in% search()) {
+    x <- mist_conflicts()
+    msg(mist_conflict_message(x), startup = TRUE)
+  }
+
+}
+
+is_attached <- function(x) {
+  paste0("package:", x) %in% search()
+}
+
+#' Conflicts between the mist and other packages
+#'
+#' This function lists all the conflicts between packages in the mist
+#' and other packages that you have loaded.
+#'
+#' There are four conflicts that are deliberately ignored: \code{intersect},
+#' \code{union}, \code{setequal}, and \code{setdiff} from dplyr. These functions
+#' make the base equivalents generic, so shouldn't negatively affect any
+#' existing code.
+#'
+#' @export
+#' @examples
+#' mist_conflicts()
+mist_conflicts <- function() {
+  envs <- grep("^package:", search(), value = TRUE)
+  envs <- purrr::set_names(envs)
+  objs <- invert(lapply(envs, ls_env))
+
+  conflicts <- purrr::keep(objs, ~ length(.x) > 1)
+
+  tidy_names <- paste0("package:", mist_packages())
+  conflicts <- purrr::keep(conflicts, ~ any(.x %in% tidy_names))
+
+  conflict_funs <- purrr::imap(conflicts, confirm_conflict)
+  conflict_funs <- purrr::compact(conflict_funs)
+
+  structure(conflict_funs, class = "mist_conflicts")
+}
+
+mist_conflict_message <- function(x) {
+  if (length(x) == 0) return("")
+
+  header <- cli::rule(
+    left = crayon::bold("Conflicts"),
+    right = "mist_conflicts()"
+  )
+
+  pkgs <- x %>% purrr::map(~ gsub("^package:", "", .))
+  others <- pkgs %>% purrr::map(`[`, -1)
+  other_calls <- purrr::map2_chr(
+    others, names(others),
+    ~ paste0(crayon::blue(.x), "::", .y, "()", collapse = ", ")
+  )
+
+  winner <- pkgs %>% purrr::map_chr(1)
+  funs <- format(paste0(crayon::blue(winner), "::", crayon::green(paste0(names(x), "()"))))
+  bullets <- paste0(
+    crayon::red(cli::symbol$cross), " ", funs,
+    " masks ", other_calls,
+    collapse = "\n"
+  )
+
+  paste0(header, "\n", bullets)
+}
+
+#' @export
+print.mist_conflicts <- function(x, ..., startup = FALSE) {
+  cli::cat_line(mist_conflict_message(x))
+}
+
+#' @importFrom magrittr %>%
+confirm_conflict <- function(packages, name) {
+  # Only look at functions
+  objs <- packages %>%
+    purrr::map(~ get(name, pos = .)) %>%
+    purrr::keep(is.function)
+
+  if (length(objs) <= 1)
+    return()
+
+  # Remove identical functions
+  objs <- objs[!duplicated(objs)]
+  packages <- packages[!duplicated(packages)]
+  if (length(objs) == 1)
+    return()
+
+  packages
+}
+
+ls_env <- function(env) {
+  x <- ls(pos = env)
+  if (identical(env, "package:dplyr")) {
+    x <- setdiff(x, c("intersect", "setdiff", "setequal", "union"))
+  }
+  x
+}
+
+msg <- function(..., startup = FALSE) {
+  if (startup) {
+    if (!isTRUE(getOption("carot.quiet"))) {
+      packageStartupMessage(text_col(...))
+    }
+  } else {
+    message(text_col(...))
+  }
+}
+
+text_col <- function(x) {
+  # If RStudio not available, messages already printed in black
+  if (!rstudioapi::isAvailable()) {
+    return(x)
+  }
+
+  if (!rstudioapi::hasFun("getThemeInfo")) {
+    return(x)
+  }
+
+  theme <- rstudioapi::getThemeInfo()
+
+  if (isTRUE(theme$dark)) crayon::white(x) else crayon::black(x)
+
+}
+
+mist_packages <- function(include_self = TRUE) {
+  raw <- utils::packageDescription("CARoT")$Imports
+  imports <- strsplit(raw, ",")[[1]]
+  parsed <- gsub("^\\s+|\\s+$", "", imports)
+  names <- vapply(strsplit(parsed, "\\s+"), "[[", 1, FUN.VALUE = character(1))
+
+  if (include_self) {
+    names <- c(names, "carot")
+  }
+
+  names
+}
+
+invert <- function(x) {
+  if (length(x) == 0) return()
+  stacked <- utils::stack(x)
+  tapply(as.character(stacked$ind), stacked$values, list)
+}
+
+
+style_grey <- function(level, ...) {
+  crayon::style(
+    paste0(...),
+    crayon::make_style(grDevices::grey(level), grey = TRUE)
+  )
+}
